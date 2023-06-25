2701ba2ed74dd91e3a26885fa41aaa5097d157d0
guilherme-aguiar <gui9627oli@gmail.com>
2023-06-25 19:01:49 -0300

    fix: env variable retrieval

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
+import os
+
 from flask import Flask, jsonify, json
 from flask_restful import Resource, Api
 from app.api import api
 from app.config import config_by_name
 from app.controller import ParsePDFAndRunPrompt
-
 from flask_swagger_ui import get_swaggerui_blueprint
+from dotenv import load_dotenv, find_dotenv
 
-def create_app(config_name='dev'):
+def create_app():
     app = Flask(__name__)
     # Sets file upload limit 16MB
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
@@ -17,14 +19,15 @@ def create_app(config_name='dev'):
 
     api.add_resource(HealthCheck, '/')
 
-    app.config.from_object(config_by_name[config_name])
+    load_dotenv(find_dotenv())
+    app.config.from_object(config_by_name[os.getenv('ENV_TYPE')])
 
     # Flask API Initialization
     api.init_app(app)   
 
     # Configure Swagger UI
     SWAGGER_URL = '/swagger'
-    API_URL = 'http://localhost:5000/swagger.json'
+    API_URL = f'http://{os.getenv("SWAGGER_HOST")}:5000/swagger.json'
     swaggerui_blueprint = get_swaggerui_blueprint(
         SWAGGER_URL,
         API_URL,
